0719e2a4276e75b35bd0ae75b182c9f59195266c
Archher <archerske@gmail.com>
2021-04-11 17:44:34 -0400

    Update calculations.m

--- calculations.m
+++ calculations.m
@@ -23,7 +23,7 @@ cableSpeed = minDrumOD*pi*motorRPM/60 %in/sec
 liftTime = pulleyRatio * liftHeight/cableSpeed %seconds
 
 J = (pi/32) * minDrumOD^4
-TorsionalShearStress = (cableTension* cableSF * (minDrumOD/2))/J
+TorsionalShearStressDrum = (cableTension* cableSF * (minDrumOD/2))/J
 
 beamLength = 80*12; %inches
 stDensity = 0.284; %lb/in^3
@@ -32,10 +32,13 @@ alDensity = 0098; %lb/in^3
 beamArea = (insert_area); %inches^2
 beamVolume = beamLength*beamArea; %inches^3
 beamWeight = beamVolume*beamDensity; %lb
+beamMOI = (insert_MOI);
+beamE = (insert_moduluselasticity);
 
-A=?
-V=80*12*A
-W = V*.284
-Wo=W/80
-x=?
-V=-Wo(x)^1 -(2000+100000*cableSF)/2 *(x-78.5)^0
+distributedWeight=beamWeight/beamLength
+syms x
+ShearForce=-distributedWeight*(x)^1 -(2000+100000*cableSF)/2 *(x-942)^0
+
+beamSag = (-5*(distributedWeight)*beamLength^4)/(384*beamE*beamMOI)+(-2000*beamLength^3)/(48*beamE*beamMOI)
+beamVerticalDeflection = (-5*(distributedWeight)*beamLength^4)/(384*beamE*beamMOI)+(-202000*beamLength^3)/(48*beamE*beamMOI)
+beamSlope = (-1*distributedWeight*beamLength^3)/(24*beamE*beamMOI)+(-202000*beamLength^2)/(16*beamE*beamMOI)
