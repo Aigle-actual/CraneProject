f2b1a1e7603761bfc9a07b92e6fcf2dcc2ceb6ef
Archher <archerske@gmail.com>
2021-04-12 22:08:02 -0400

    Added area, MOI, q, and shear stress calculators

--- calculations.m
+++ calculations.m
@@ -29,17 +29,24 @@ beamLength = 80*12; %inches
 stDensity = 0.284; %lb/in^3
 alDensity = 0.098; %lb/in^3
 
-beamArea = 256; %inches^2
+beamWidth= 12; %inches
+beamFlange = 1.02; %inches
+beamHeight = 36.01; %inches
+beamWeb = .65; %inches
+b= beamHeight/2
+Q = ((beamHeight-2*beamFlange)/2)*(beamWeb)*(((beamHeight-2*beamFlange)/2)/2)+beamFlange*beamWidth*(beamHeight/2-beamFlange/2)
+beamArea = (beamWidth*beamHeight)-((beamHeight - 2*beamFlange)*(beamWidth-beamWeb)); %inches^2
 beamVolume = beamLength*beamArea; %inches^3
 beamDensity = stDensity;
 beamWeight = beamVolume*beamDensity; %lb
-beamMOI = 54261.3;
+beamMOI = (1/12)*((beamWidth*beamHeight^3)-(beamWidth-beamWeb)*(beamHeight-(2*beamFlange))^3);
 beamE = 30000000;
 
 distributedWeight=(beamWeight*1.2)/beamLength
-syms x
-ShearForce=-distributedWeight*(x)^1 -(2000+100000*cableSF)/2 *(x-942)^0
+x = 80
+shearForce=-distributedWeight*(x)^1 -(1000+50000*cableSF)/2 *(x-942)^0
+shearStress = (shearForce * Q)/(beamMOI * b)
 
-beamSag = (-5*(distributedWeight)*beamLength^4)/(384*beamE*beamMOI)+(-2000*beamLength^3)/(48*beamE*beamMOI)
-beamVerticalDeflection = (-5*(distributedWeight)*beamLength^4)/(384*beamE*beamMOI)+(-102000*beamLength^3)/(48*beamE*beamMOI)
-beamSlope = (-1*distributedWeight*beamLength^3)/(24*beamE*beamMOI)+(-102000*beamLength^2)/(16*beamE*beamMOI)
+beamSag = (-5*(distributedWeight)*beamLength^4)/(384*beamE*beamMOI)+(-1000*beamLength^3)/(48*beamE*beamMOI)
+beamVerticalDeflection = (-5*(distributedWeight)*beamLength^4)/(384*beamE*beamMOI)+(-51000*beamLength^3)/(48*beamE*beamMOI)
+beamSlope = (-1*distributedWeight*beamLength^3)/(24*beamE*beamMOI)+(-51000*beamLength^2)/(16*beamE*beamMOI)
